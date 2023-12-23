1e702e0e721fa8cf007205653b10de34f0aa63fd
Deep Patel <deeppatel.ce@gmail.com>
2023-12-23 13:30:10 -0500

    Added PlatformIconList component to render icons for game platforms. Added react-icons npm package to map icons with slug information from the API

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
     game: Game;
@@ -12,6 +13,9 @@ const GameCard = ({ game }: Props) => {
                 <Image src={game.background_image}></Image>
                 <CardBody>
                     <Heading fontSize="2xl">{game.name}</Heading>
+                    <PlatformIconList
+                        platforms={game.parent_platforms.map((p) => p.platform)}
+                    ></PlatformIconList>
                 </CardBody>
             </Card>
         </>
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,48 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import {
+    FaWindows,
+    FaPlaystation,
+    FaXbox,
+    FaApple,
+    FaLinux,
+    FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+    platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap: { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+        android: FaAndroid,
+    };
+
+    return (
+        <>
+            <HStack marginY={2}>
+                {platforms.map((platform) => (
+                    <Icon
+                        key={platform.id}
+                        as={iconMap[platform.slug]}
+                        color="gray"
+                    ></Icon>
+                ))}
+            </HStack>
+        </>
+    );
+};
+
+export default PlatformIconList;
