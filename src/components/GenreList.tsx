dbd0d31fc19fd2aa717617265ab27e15362468c8
Deep Patel <deeppatel.ce@gmail.com>
2023-12-23 18:44:03 -0500

    Filter games on selecting genres in the side panel.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,20 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+    Button,
+    HStack,
+    Image,
+    List,
+    ListItem,
+    Spinner,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+    selectedGenre: Genre | null;
+    onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
     const { data, error, isLoading } = useGenres();
 
     if (error) return null;
@@ -19,7 +31,18 @@ const GenreList = () => {
                                 borderRadius={8}
                                 src={getCroppedImageUrl(genre.image_background)}
                             />
-                            <Text fontSize="lg">{genre.name}</Text>
+                            <Button
+                                fontWeight={
+                                    genre.id === selectedGenre?.id
+                                        ? "bold"
+                                        : "normal"
+                                }
+                                fontSize="lg"
+                                variant="link"
+                                onClick={() => onSelectGenre(genre)}
+                            >
+                                {genre.name}
+                            </Button>
                         </HStack>
                     </ListItem>
                 ))}
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { AxiosError, CanceledError } from "axios";
+import { AxiosError, AxiosRequestConfig, CanceledError } from "axios";
 
 interface GetResponse<T> {
     count: number;
     results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, dependencies?: any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setLoading] = useState(false);
@@ -17,7 +17,7 @@ const useData = <T>(endpoint: string) => {
 
         setLoading(true);
         apiClient
-            .get<GetResponse<T>>(endpoint, { signal: controller.signal })
+            .get<GetResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
             .then((res) => {
                 setData(res.data.results);
                 setLoading(false);
@@ -29,7 +29,7 @@ const useData = <T>(endpoint: string) => {
             })
 
         return () => controller.abort();
-    }, []);
+    }, dependencies ? [...dependencies] : []);
 
     return { data, error, isLoading };
 }
