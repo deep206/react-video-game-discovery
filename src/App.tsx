8aa4d36fc102db89ef687bbd698ab746ebe6a82b
Deep Patel <deeppatel.ce@gmail.com>
2023-12-23 10:09:14 -0500

    Setup grid container using ChakraUI for responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-    return <Button colorScheme="blue">Button</Button>;
+    return (
+        <Grid
+            templateAreas={{
+                base: `"nav" "main"`, //base for mobile
+                lg: `"nav nav" "aside main"`, //for screens larger than 1024px
+            }}
+        >
+            <GridItem area={"nav"} bg="red">
+                nav
+            </GridItem>
+            <Show above="lg">
+                <GridItem area={"aside"} bg="yellow">
+                    aside
+                </GridItem>
+            </Show>
+            <GridItem area={"main"} bg="green">
+                main
+            </GridItem>
+        </Grid>
+    );
 }
 
 export default App;
